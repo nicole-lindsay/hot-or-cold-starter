3b7cdd3e31ab53bee154305a75037048684a08a3
nicole-lindsay <nicoleyasdnil@gmail.com>
2016-06-14 22:13:01 -0400

    added random number generator

--- js/app.js
+++ js/app.js
@@ -12,4 +12,16 @@ $(document).ready(function(){
   		$(".overlay").fadeOut(1000);
   	});
 
+  	/*random number generator*/
+  	Math.floor((Math.random() * 100) + 1);
+
+  	/*---New game button---*/
+  	$(".new").click(function(){
+  		/*needs to revert #feedback, #count, #guessList to original values, and change random number*/ 
+  	});
+
+  	/*Guess button*/
+  	$("#guessButton").click(function(){
+  		/*needs to add <li> to #guesslist, increment #count by one, change #feedback to hot/cold or you win*/
+  	});
 });
