ccbd59b429d6690b1deb7699ed0fea0a7fb07c9a
nicole-lindsay <nicoleyasdnil@gmail.com>
2016-06-19 20:50:20 -0400

    fixed syntax error

--- js/app.js
+++ js/app.js
@@ -1,3 +1,4 @@
+var guessCount = 0;
 $(document).ready(function() {
 
     /*--- Display information modal box ---*/
@@ -13,33 +14,45 @@ $(document).ready(function() {
 
     /*variables reference*/
     var randomNumber = Math.floor((Math.random() * 100) + 1);
-    console.log(randomNumber);
+    console.log("The random number is " + randomNumber);
+
 
     /*---New game button---*/
     $(".new").click(function() {
         event.preventDefault();
-        /*needs to revert #feedback, #count, #guessList to original values, and change random number*/
+        $("#feedback").html("Make your Guess!");
+        $("#count").html(0);
+        guessCount = 0;
+        $("#guessList li").remove();
+        randomNumber = Math.floor((Math.random() * 100) + 1);
+        console.log("The random number is " + randomNumber);
     });
 
-    $("form").on("submit", function(event) {
-        event.preventDefault();
-    });
-
-    /*Guess button, increment #count by one*/
-    $("#guessButton").click(function() {
+    /*Guess button*/
+    //$("#guessButton").click(function(event) {
+    //$("form").on("submit", function(event) {
+    $("#guessButton").on("click", function(event) {
         event.preventDefault();
         var userGuess = $("#userGuess").val();
         var difference = Math.abs(randomNumber - userGuess);
-        $("#guessList").append("<li>" + userGuess + "</li>");
         $("#userGuess").val("");
-        if (userGuess == randomNumber) {
-            $("#feedback").text("Winner winner chicken dinner!");
-        } else if (difference >= 1 && difference <= 30) {
-            $("#feedback").text("Hot!");
-        } else if (difference >= 31 && difference <= 70) {
-            $("#feedback").text("Tepid");
+        if (isNaN(userGuess)) {
+            $("#feedback").text("No dice, I only accept numbers!");
+        } else if (userGuess < 0 && userGuess > 100) {
+            $("#feedback").text("Only numbers 1-100!");
         } else {
-            $("#feedback").text("Cold!");
+            guessCount++
+            $("#count").text(guessCount);
+            $("#guessList").append("<li>" + userGuess + "</li>");
+            if (userGuess == randomNumber) {
+                $("#feedback").text("Winner winner chicken dinner!");
+            } else if (difference >= 1 && difference <= 30) {
+                $("#feedback").text("Hot!");
+            } else if (difference >= 31 && difference <= 70) {
+                $("#feedback").text("Tepid");
+            } else {
+                $("#feedback").text("Cold");
+            }
         }
     });
 });
